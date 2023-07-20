ff6d8b794fccb5b7bba21fba73b25d08c11d6086
Иван Далеченко <ivandalechenko@gmail.com>
2023-07-20 04:46:10 +0300

    chart

--- src/components/content/stat/Balance.jsx
+++ src/components/content/stat/Balance.jsx
@@ -4,8 +4,41 @@ import ChangeTarifCheckStatus from '../../UI/ChangeTarifCheckStatus';
 const Balance = (props) => {
     return (
         <div className="section" id="balance">
-            <div className="header h5">
-                Your balance
+            <div className="header ">
+                <div className="name h5">
+                    Your balance
+                </div>
+                <div className="period">
+                    <div className="text">
+                        Select period
+                    </div>
+                    <div className="elements">
+                        <div className="element">
+                            1h
+                        </div>
+                        <div className="element">
+                            3h
+                        </div>
+                        <div className="element active">
+                            1d
+                        </div>
+                        <div className="element">
+                            3d
+                        </div>
+                        <div className="element">
+                            7d
+                        </div>
+                        <div className="element">
+                            2w
+                        </div>
+                        <div className="element">
+                            1m
+                        </div>
+                        <div className="element">
+                            1y
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="hr">
             </div>
--- src/components/content/stat/StatPage.jsx
+++ src/components/content/stat/StatPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import Sidebar from '../../sidebar/Sidebar';
 import HorizontalLine from '../../UI/HorizontalLine';
 import Balance from './Balance';
+import Chart from './Chart';
 
 const StatPage = () => {
     useEffect(() => {
@@ -12,14 +13,9 @@ const StatPage = () => {
             <Sidebar />
             <div id='content'>
                 <div id="ref_page">
-                    {/* <div className="section" id="ref">
-                        <div className="header h5">
-                            Coming soon :)
-                        </div>
-                        <HorizontalLine />
-                    </div> */}
-                    <Balance />
 
+                    <Balance />
+                    <Chart />
                 </div>
             </div>
         </div>
